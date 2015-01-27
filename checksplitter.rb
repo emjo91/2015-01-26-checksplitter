8d817f202fc553e97a412a4923214df06af032b9
EmilyJohnson <3mle33@gmail.com>
2015-01-26 20:06:24 -0600

    added some setter methods and more text at the bottom!

--- checksplitter.rb
+++ checksplitter.rb
@@ -7,8 +7,20 @@ class CheckSplit
     @tip_amount = @meal_cost * 0.15
   end
   
-  def get_tip
+  def get_tip (param_three)
+    @tip_amount = param_three
+  end
+  
+  def set_tip
     @tip_amount
+  end
+  
+  def set_people_amount
+    @people_amount
+  end
+  
+  def set_meal_cost
+    @meal_cost
   end
   
   def meal_and_tip
@@ -20,4 +32,13 @@ class CheckSplit
   end
 end
 
-party_one = CheckSplit.new (4, 40)
+party_one = CheckSplit.new(4, 40.35)
+party_one.get_tip(15.15)
+puts "Since your meal is #{party_one.set_meal_cost}, and there are #{party_one.set_people_amount} of you, and you're tipping $#{party_one.set_tip}..."
+puts "...that means you each owe $#{party_one.price_divide}."
+puts
+party_two = CheckSplit.new(6, 122.64)
+puts "Since your meal is #{party_two.set_meal_cost}, and there are #{party_two.set_people_amount} of you, and you're tipping $#{party_two.set_tip}..."
+puts "...that means you each owe $#{party_two.price_divide}."
+
+binding.pry
