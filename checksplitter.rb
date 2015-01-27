7f4c7ee63401158186e652c9a1ac0eeea79b2a9d
EmilyJohnson <3mle33@gmail.com>
2015-01-27 09:05:44 -0600

    added a getter method for people_amount

--- checksplitter.rb
+++ checksplitter.rb
@@ -15,6 +15,10 @@ class CheckSplit
     @tip_amount
   end
   
+  def get_people_amount
+    @people_amount
+  end 
+  
   def set_people_amount
     @people_amount
   end
@@ -34,11 +38,11 @@ end
 
 party_one = CheckSplit.new(4, 40.35)
 party_one.get_tip(15.15)
-puts "Since your meal is #{party_one.set_meal_cost}, and there are #{party_one.set_people_amount} of you, and you're tipping $#{party_one.set_tip}..."
+puts "Since your meal is $#{party_one.set_meal_cost}, and there are #{party_one.set_people_amount} of you, and you're tipping $#{party_one.set_tip}..."
 puts "...that means you each owe $#{party_one.price_divide}."
 puts
 party_two = CheckSplit.new(6, 122.64)
-puts "Since your meal is #{party_two.set_meal_cost}, and there are #{party_two.set_people_amount} of you, and you're tipping $#{party_two.set_tip}..."
+puts "Since your meal is $#{party_two.set_meal_cost}, and there are #{party_two.set_people_amount} of you, and you're tipping $#{party_two.set_tip}..."
 puts "...that means you each owe $#{party_two.price_divide}."
 
 #seems to work fine, just need to round out those decimals...
