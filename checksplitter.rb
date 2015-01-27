8881036895bfcc8dcc436699ca9b66d33279ca97
EmilyJohnson <3mle33@gmail.com>
2015-01-26 19:04:22 -0600

    Added the initialize methods!

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,5 +1,9 @@
 require "pry"
 
 class CheckSplit
-  
+  def initialize (param_one, param_two, param_three)
+    @people_amount = param_one
+    @meal_cost = param_two
+    @tip_amount = param_three
+  end
 end
