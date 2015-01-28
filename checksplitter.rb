b8d31bc0d9257d4e3e80deb1d2ab91e3ffc4265c
EmilyJohnson <3mle33@gmail.com>
2015-01-27 20:33:54 -0600

    Having a TON of trouble with this... I'm not having much luck getting people added...

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,23 +1,38 @@
 require "pry"
 
+class DinnerClub
+
+def initialize (param1)
+  @members = param1
+  @total_amount = {}
+end 
+
+def add_members (name, balance)
+  @members.push (name: name, balance: CheckSplitter.new) #not sure if this is a legal move
+end 
+
+def total_amount #not working
+  balance = 0.0
+  @transactions
+end
+
+end
+
+#second class
 class CheckSplit
   def initialize (param_one, param_two)
     @people_amount = param_one
     @meal_cost = param_two
-    @tip_amount = @meal_cost * 0.15
+    @tip_amount = @meal_cost * 0.30
   end
   
   def get_tip (param_three)
-    @tip_amount = param_three
+    @tip_amount = param_three 
   end
   
   def set_tip
-    @tip_amount
+    @tip_amount * 0.01
   end
-  
-  def get_people_amount
-    @people_amount
-  end 
   
   def set_people_amount
     @people_amount
@@ -28,7 +43,7 @@ class CheckSplit
   end
   
   def meal_and_tip
-    @tip_amount + @meal_cost
+    set_tip_amount + @meal_cost
   end
   
   def price_divide
@@ -36,6 +51,21 @@ class CheckSplit
   end
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 party_one = CheckSplit.new(4, 40.35)
 party_one.get_tip(15.15)
 puts "Since your meal is $#{party_one.set_meal_cost}, and there are #{party_one.set_people_amount} of you, and you're tipping $#{party_one.set_tip}..."
@@ -44,7 +74,7 @@ puts
 party_two = CheckSplit.new(6, 122.64)
 puts "Since your meal is $#{party_two.set_meal_cost}, and there are #{party_two.set_people_amount} of you, and you're tipping $#{party_two.set_tip}..."
 puts "...that means you each owe $#{party_two.price_divide}."
-
+=end
 #seems to work fine, just need to round out those decimals...
 
 binding.pry
